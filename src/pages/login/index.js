db01e18e2c07d5b16a6a78b8b389e1bdfc598ea2
namtq184161 <quangnam260800@gmail.com>
2022-12-11 17:55:59 +0700

    install formik, yup + login page fronted

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,3 +1,48 @@
+import { Form, Formik } from 'formik';
+import { Link } from 'react-router-dom';
+
+import './style.css';
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div clasName="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../../icons/facebook.svg" alt="" />
+            <span>
+              Aimer helps you connect and share with the people in your life.
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik>
+                {(formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button className="blue_btn" type="submit">
+                      Log in
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+              <Link className="forgot_password" to="/forgot">
+                Forgotten password?
+              </Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">
+                Create New Account
+              </button>
+            </div>
+            <Link to="/" className="sign_extra">
+              <b>Create a Page</b> for a celebrity, brand or business.
+            </Link>
+          </div>
+        </div>
+
+        <div className="register_wrap"></div>
+      </div>
+    </div>
+  );
 }
